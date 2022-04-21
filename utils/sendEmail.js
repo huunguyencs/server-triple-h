95895e865f4f3bcca224d106505f9396431aa03b
Huu Nguyen <thieuvan0405@gmail.com>
2022-04-21 15:59:14 +0700

    log send mail

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,46 +1,46 @@
 const nodemailer = require('nodemailer');
-const {google} = require('googleapis');
-const {OAuth2} = google.auth;
-const OAUTH_PLAYGROUND = "https://developers.google.com/oauthplayground"
+const { google } = require('googleapis');
+const { OAuth2 } = google.auth;
+const OAUTH_PLAYGROUND = 'https://developers.google.com/oauthplayground';
 
 const {
-    MAILING_SERVICE_CLIENT_ID,
-    MAILING_SERVICE_CLIENT_SECRET,
-    MAILING_SERVICE_REFRESH_TOKEN,
-    SENDER_EMAIL_ADDRESS
-} = process.env
+  MAILING_SERVICE_CLIENT_ID,
+  MAILING_SERVICE_CLIENT_SECRET,
+  MAILING_SERVICE_REFRESH_TOKEN,
+  SENDER_EMAIL_ADDRESS
+} = process.env;
 
 const oauth2Client = new OAuth2(
-    MAILING_SERVICE_CLIENT_ID,
-    MAILING_SERVICE_CLIENT_SECRET,
-    MAILING_SERVICE_REFRESH_TOKEN,
-    OAUTH_PLAYGROUND
-)
+  MAILING_SERVICE_CLIENT_ID,
+  MAILING_SERVICE_CLIENT_SECRET,
+  MAILING_SERVICE_REFRESH_TOKEN,
+  OAUTH_PLAYGROUND
+);
 
 //send Email
 const sendEmail = (to, url, txt) => {
-    oauth2Client.setCredentials({
-        refresh_token: MAILING_SERVICE_REFRESH_TOKEN
-    })
+  oauth2Client.setCredentials({
+    refresh_token: MAILING_SERVICE_REFRESH_TOKEN
+  });
 
-    const accessToken = oauth2Client.getAccessToken()
-    const smtpTransport = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            type: 'OAuth2',
-            user: SENDER_EMAIL_ADDRESS,
-            clientId: MAILING_SERVICE_CLIENT_ID,
-            clientSecret: MAILING_SERVICE_CLIENT_SECRET,
-            refreshToken: MAILING_SERVICE_REFRESH_TOKEN,
-            accessToken
-        }
-    })
+  const accessToken = oauth2Client.getAccessToken();
+  const smtpTransport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: SENDER_EMAIL_ADDRESS,
+      clientId: MAILING_SERVICE_CLIENT_ID,
+      clientSecret: MAILING_SERVICE_CLIENT_SECRET,
+      refreshToken: MAILING_SERVICE_REFRESH_TOKEN,
+      accessToken
+    }
+  });
 
-    const mailOptions = {
-        from: SENDER_EMAIL_ADDRESS,
-        to: to,
-        subject: `TripleH, ${txt}`,
-        html: `
+  const mailOptions = {
+    from: SENDER_EMAIL_ADDRESS,
+    to: to,
+    subject: `TripleH, ${txt}`,
+    html: `
             <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
             <h2 style="text-align: center; text-transform: uppercase;color: teal;"> Chào mừng bạn đến với Mạng xã hội du lịch TripleH.</h2>
             <p>Xin chúc mừng! Bạn sắp trở thành thành viên của TripleH. Bạn cần nhấp vào nút bên dưới để xác thực địa chỉ email
@@ -53,12 +53,15 @@ const sendEmail = (to, url, txt) => {
             <div>${url}</div>
             </div>
         `
+  };
+
+  smtpTransport.sendMail(mailOptions, (err, infor) => {
+    if (err) {
+      console.log(err);
+      return err;
     }
+    return infor;
+  });
+};
 
-    smtpTransport.sendMail(mailOptions, (err, infor) => {
-        if(err) return err;
-        return infor
-    })
-}
-
-module.exports = sendEmail
+module.exports = sendEmail;
